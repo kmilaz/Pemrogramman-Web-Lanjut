7246460562fdfa72b554ade71d2e48dd58933460
Kamila Zahwa <kamilazhwa@gmail.com>
2026-02-24 21:21:28 +0700

    feat: langkah 7 praktikum 2

--- PWL_2026/routes/web.php
+++ PWL_2026/routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PageController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/hello', [WelcomeController::class, 'hello']);
 
@@ -12,7 +15,7 @@ Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/about', [PageController::class, 'about']);
+Route::get('/about', [AboutController::class, 'about']);
 
 Route::get('/user/{name}', function ($name) {
     return 'Nama saya ' . $name;
@@ -22,7 +25,7 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Post ke-: ' . $postId . ' Komentar ke-: ' . $commentId;
 });
 
-Route::get('/articles/{id}', [PageController::class, 'articles']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
 
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya ' . $name;
