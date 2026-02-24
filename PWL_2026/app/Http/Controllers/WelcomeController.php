e239a0d246a30e3e5e64bdd7fd32ab5cf88591b4
Kamila Zahwa <kamilazhwa@gmail.com>
2026-02-24 21:51:58 +0700

    feat: langkah 11 praktikum 3

--- PWL_2026/app/Http/Controllers/WelcomeController.php
+++ PWL_2026/app/Http/Controllers/WelcomeController.php
@@ -11,6 +11,8 @@ class WelcomeController extends Controller
     }
 
     public function greeting() {
-        return view('blog.hello', ['name' => 'Kamila']);
+        return view('blog.hello')
+            ->with('name', 'Kamila Zahwa')
+            ->with('occupation', 'Student');
     }
 }
