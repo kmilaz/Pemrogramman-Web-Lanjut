012c34ab2c07d577597bb699e7771fe00d18e9b5
Kamila Zahwa <kamilazhwa@gmail.com>
2026-03-01 22:50:54 +0700

    feat: langkah 8 praktikum 6

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -14,6 +15,14 @@ class UserController extends Controller
 
     public function index()
     {
+        $data = [
+            'username' => 'customer-1',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'),
+            'level_id' => 4
+        ];
+        UserModel::insert($data);
+        
         $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
