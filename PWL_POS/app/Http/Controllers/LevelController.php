6b040b79cfce916f7d954700472570f825e5af62
Kamila Zahwa <kamilazhwa@gmail.com>
2026-03-01 22:16:56 +0700

    feat: langkah 11 praktikum 4

--- PWL_POS/app/Http/Controllers/LevelController.php
+++ PWL_POS/app/Http/Controllers/LevelController.php
@@ -15,7 +15,10 @@ class LevelController extends Controller
         // $row = DB::update('UPDATE m_level SET level_nama = ? WHERE level_kode = ?', ['Customer', 'CUS']);
         // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row. ' baris.';
         
-        $row = DB::delete('DELETE FROM m_level WHERE level_kode = ?', ['CUS']);
-        return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row. ' baris.';
+        // $row = DB::delete('DELETE FROM m_level WHERE level_kode = ?', ['CUS']);
+        // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row. ' baris.';
+
+        $data = DB::select('SELECT * FROM m_level');
+        return view('level', ['data' => $data]);
     }
 }
