ae30b863d5c7d8c16e36786f22bbaf0cfc2bd50d
Kamila Zahwa <kamilazhwa@gmail.com>
2026-03-01 22:07:52 +0700

    feat: langkah 5 praktikum 4

--- PWL_POS/app/Http/Controllers/LevelController.php
+++ PWL_POS/app/Http/Controllers/LevelController.php
@@ -9,7 +9,10 @@ class LevelController extends Controller
 {
     public function index()
     {
-        DB::insert('INSERT INTO m_level (level_kode, level_nama, created_at) VALUES (?, ?, ?)', ['CUS', 'Pelanggan', now()]);
-        return 'Insert data baru berhasil';
+        // DB::insert('INSERT INTO m_level (level_kode, level_nama, created_at) VALUES (?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+        // return 'Insert data baru berhasil';
+
+        $row = DB::update('UPDATE m_level SET level_nama = ? WHERE level_kode = ?', ['Customer', 'CUS']);
+        return 'Update data berhasil. Jumlah data yang diupdate: ' . $row. ' baris.';
     }
 }
