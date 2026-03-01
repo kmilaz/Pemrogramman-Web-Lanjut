8c679a49199164d058095bd530fb203ff484e5a4
Kamila Zahwa <kamilazhwa@gmail.com>
2026-03-01 20:36:50 +0700

    fix: change schema from table to create

--- PWL_POS/database/migrations/2026_03_01_130824_create_m_barang_table.php
+++ PWL_POS/database/migrations/2026_03_01_130824_create_m_barang_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('m_barang', function (Blueprint $table) {
+        Schema::create('m_barang', function (Blueprint $table) {
             $table->id('barang_id');
             $table->unsignedBigInteger('kategori_id')->index();
             $table->string('barang_kode', 10)->unique();
