3dbc7b8f23685c29e7d618b3ed32f08bc895b748
Kamila Zahwa <kamilazhwa@gmail.com>
2026-03-01 20:44:43 +0700

    fix: drop if exist

--- PWL_POS/database/migrations/2026_03_01_130824_create_m_barang_table.php
+++ PWL_POS/database/migrations/2026_03_01_130824_create_m_barang_table.php
@@ -29,8 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('m_barang', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('m_barang');
     }
 };
