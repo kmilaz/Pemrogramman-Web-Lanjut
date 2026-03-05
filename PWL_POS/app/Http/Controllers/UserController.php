bb4e72f6bb64b5be472edb665ad33efb2d1a24f1
Kamila Zahwa <kamilazhwa@gmail.com>
2026-03-05 21:36:50 +0700

    feat: langkah 3 praktikum 2.2

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
 
     public function index()
     {
-        $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
