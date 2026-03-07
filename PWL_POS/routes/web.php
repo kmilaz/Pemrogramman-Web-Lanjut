e16f3cb6ad5998a48c3c804b96c4fcf5fe5c7a8b
Kamila Zahwa <kamilazhwa@gmail.com>
2026-03-07 20:16:57 +0700

    feat: langkah 5 praktikum 2.6

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -26,3 +26,5 @@ Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 
 Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/user/tambah', [UserController::class, 'tambah']);
