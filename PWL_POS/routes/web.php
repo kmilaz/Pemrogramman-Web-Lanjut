11b7e7154674ce828af217a9ce35362a7fd03f77
Kamila Zahwa <kamilazhwa@gmail.com>
2026-03-07 22:44:30 +0700

    feat: langkah 8 praktikum 2.6

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -28,3 +28,5 @@ Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
 
 Route::get('/user/tambah', [UserController::class, 'tambah']);
+
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
