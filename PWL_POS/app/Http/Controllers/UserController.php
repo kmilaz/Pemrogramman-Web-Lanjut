2fbef776160d68afe6a531031757a565de1e57c2
Kamila Zahwa <kamilazhwa@gmail.com>
2026-03-07 19:39:57 +0700

    feat: langkah 3 praktikum 2.5

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -18,28 +18,20 @@ class UserController extends Controller
     public function index()
     {
         $user = UserModel::create([
-            'username' => 'manager55',
-            'nama' => 'Manager55',
+            'username' => 'manager11',
+            'nama' => 'Manager11',
             'password' => Hash::make('12345'),
             'level_id' => 2,
         ]);
 
-        $user->username = 'manager56';
-
-        $user->isDirty();
-        $user->isDirty('username');
-        $user->isDirty('nama');
-        $user->isDirty(['nama', 'username']);
-
-        $user->isClean();
-        $user->isClean('username');
-        $user->isClean('nama');
-        $user->isClean(['nama', 'username']);
+        $user->username = 'manager12';
 
         $user->save();
 
-        $user->isDirty();
-        $user->isClean();
-        dd($user->isDirty());
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama', 'username']));
     }
 }
