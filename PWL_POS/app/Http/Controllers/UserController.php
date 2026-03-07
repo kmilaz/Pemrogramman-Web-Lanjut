d6f19d84be99bd21df35f94ee93d746797d94dd3
Kamila Zahwa <kamilazhwa@gmail.com>
2026-03-08 00:05:49 +0700

    feat: langkah 4 praktikum 2.7

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
     public function index()
     {
         $user = UserModel::with('level')->get();
-        dd($user);
+        return view('user', ['data' => $user]);
     }
 
     public function tambah()
