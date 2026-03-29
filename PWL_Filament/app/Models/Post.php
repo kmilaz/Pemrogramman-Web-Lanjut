c7a968e150216ad1357ecbb963acb668376c20aa
Kamila Zahwa <kamilazhwa@gmail.com>
2026-03-29 18:43:20 +0700

    feat: add fillable and casts for Post model

--- PWL_Filament/app/Models/Post.php
+++ PWL_Filament/app/Models/Post.php
@@ -6,4 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = [
+        'title',
+        'slug',
+        'category_id',
+        'color',
+        'image',
+        'body',
+        'tags',
+        'published',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'tags' => 'array',
+        'published' => 'boolean',
+        'published_at' => 'date',
+    ];
 }
