ad8bad08821e0992d9298f976d46183abcf23ad2
Kamila Zahwa <kamilazhwa@gmail.com>
2026-04-08 22:28:25 +0700

    feat: post table columns

--- PWL_Filament/app/Filament/Resources/Posts/Tables/PostsTable.php
+++ PWL_Filament/app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -16,6 +16,12 @@ class PostsTable
     {
         return $table
             ->columns([
+                TextColumn::make("title"),
+                TextColumn::make("slug"),
+                TextColumn::make("category.name"),
+                ColorColumn::make("color"),
+                ImageColumn::make("image")
+                ->disk("public"),
             ])
             ->filters([
                 //
