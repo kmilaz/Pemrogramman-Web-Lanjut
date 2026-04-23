04f363e89017fb3523434d91e68148fe1a94ad1a
Kamila Zahwa <kamilazhwa@gmail.com>
2026-04-23 13:23:38 +0700

    feat: add product info section

--- PWL_Filament/app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ PWL_Filament/app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Resources\Products\Schemas;
 
+use Dom\Text;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class ProductInfolist
@@ -10,7 +13,23 @@ class ProductInfolist
     {
         return $schema
             ->components([
-                //
+                Section::make('Product Info')
+                    ->description('')
+                    ->schema([
+                        TextEntry::make('name')
+                            ->label('Product Name')
+                            ->weight('bold')
+                            ->color('primary'),
+                        TextEntry::make('id')
+                            ->label('Product ID'),
+                        TextEntry::make('sku')
+                            ->label('Product SKU')
+                            ->badge()
+                            ->color('success'),
+                        TextEntry::make('description')
+                            ->label('Product Description')
+                    ])
+                ->columnSpanFull()
             ]);
     }
 }
